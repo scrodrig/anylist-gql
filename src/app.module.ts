468df165559d761151a12a533e28bae56cf08863
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-06-01 00:40:09 +0100

    chore: Update npm dependencies for Apollo Server and NestJS GraphQL

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,21 @@
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { GraphQLModule } from '@nestjs/graphql';
 import { Module } from '@nestjs/common';
+import { join } from 'path';
+import { ItemsModule } from './items/items.module';
 
 @Module({
-  imports: [],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
+      plugins: [ApolloServerPluginLandingPageLocalDefault() as any],
+    }),
+    ItemsModule,
+  ],
   controllers: [],
   providers: [],
 })
