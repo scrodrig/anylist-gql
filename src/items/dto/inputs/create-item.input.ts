2ad204bcd9c9a09878f22431d9a9d63c4368d871
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-06-01 21:59:31 +0100

    refactor: Update CreateItemInput to include quantity and quantityUnits fields

--- src/items/dto/inputs/create-item.input.ts
+++ src/items/dto/inputs/create-item.input.ts
@@ -1,7 +1,19 @@
-import { InputType, Int, Field } from '@nestjs/graphql';
+import { Field, Float, InputType } from '@nestjs/graphql';
+import { IsNotEmpty, IsOptional, IsPositive, IsString } from 'class-validator';
 
 @InputType()
 export class CreateItemInput {
-  @Field(() => Int, { description: 'Example field (placeholder)' })
-  exampleField: number;
+  @Field(() => String, { description: 'The name of the item' })
+  @IsNotEmpty()
+  @IsString()
+  name: string;
+
+  @Field(() => Float, { description: 'The quantity of the item' })
+  @IsPositive()
+  quantity: number;
+
+  @Field(() => String, { description: 'Units of the quantity', nullable: true })
+  @IsString()
+  @IsOptional()
+  quantityUnits?: string;
 }
