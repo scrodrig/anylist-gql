d2bae11e40584f18a6fbf7b56f996583324784c5
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-06-01 20:43:34 +0100

    refactor: Update app.module.ts to configure TypeOrmModule for database connection

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,18 +1,31 @@
 import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
 
 import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
+import { ConfigModule } from '@nestjs/config';
 import { GraphQLModule } from '@nestjs/graphql';
-import { Module } from '@nestjs/common';
-import { join } from 'path';
 import { ItemsModule } from './items/items.module';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     GraphQLModule.forRoot<ApolloDriverConfig>({
       driver: ApolloDriver,
       playground: false,
       autoSchemaFile: join(process.cwd(), 'src/schema.gql'),
       plugins: [ApolloServerPluginLandingPageLocalDefault() as any],
+    }),
+    TypeOrmModule.forRoot({
+      type: 'postgres',
+      host: process.env.DB_HOST || 'localhost',
+      port: process.env.DB_PORT ? parseInt(process.env.DB_PORT) : 5432,
+      username: process.env.DB_USERNAME,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_NAME,
+      synchronize: true,
+      autoLoadEntities: true,
     }),
     ItemsModule,
   ],
