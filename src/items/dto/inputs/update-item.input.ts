3f70b51ca9930c2cc9257f933bd4bb600d2516ef
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-06-01 22:52:40 +0100

    refactor: Update UpdateItemInput to use ID type instead of number

--- src/items/dto/inputs/update-item.input.ts
+++ src/items/dto/inputs/update-item.input.ts
@@ -1,8 +1,12 @@
+import { Field, ID, InputType, PartialType } from '@nestjs/graphql';
+import { IsNotEmpty, IsUUID } from 'class-validator';
+
 import { CreateItemInput } from './create-item.input';
-import { InputType, Field, Int, PartialType } from '@nestjs/graphql';
 
 @InputType()
 export class UpdateItemInput extends PartialType(CreateItemInput) {
-  @Field(() => Int)
-  id: number;
+  @Field(() => ID)
+  @IsNotEmpty()
+  @IsUUID()
+  id: string;
 }
