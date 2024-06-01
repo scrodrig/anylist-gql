19668c8a6a9803be6b2f83523a9628e057f2e07c
Schubert D. Rodriguez <schubert.david.r@gmail.com>
2024-06-01 20:43:29 +0100

    refactor: Update ItemsModule to use TypeOrmModule.forFeature for Item entity

--- src/items/items.module.ts
+++ src/items/items.module.ts
@@ -1,8 +1,11 @@
-import { Module } from '@nestjs/common';
-import { ItemsService } from './items.service';
+import { Item } from './entities/item.entity';
 import { ItemsResolver } from './items.resolver';
+import { ItemsService } from './items.service';
+import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Item])],
   providers: [ItemsResolver, ItemsService],
 })
 export class ItemsModule {}
